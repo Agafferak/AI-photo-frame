45a69a2040110781ca7344a6a1c580a4b10cd200
Антон Шеффер <Agaffer321@gmail.com>
2025-01-02 00:32:40 +0300

    step0

--- gallery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Папка с изображениями
-IMAGE_DIR="/usr/Project/Images"
-
-# Запуск галереи в полном экране с периодом смены изображения в 15 секунд
-feh --fullscreen --slideshow-delay 15 "$IMAGE_DIR"
