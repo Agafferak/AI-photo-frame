14dacb72443e1963a7d2a50c07ff189df135b610
Agafferak <Agaffer321@gmail.com>
2025-04-23 19:53:09 +0300

    Update generate.sh

--- photoframe/generate.sh
+++ photoframe/generate.sh
@@ -12,15 +12,21 @@ echo "PROMPT=\"$PROMPT\"" > $CONFIG_FILE
 echo "DELAY=$DELAY" >> $CONFIG_FILE
 echo "IMAGE_DIR=\"$IMAGE_DIR\"" >> $CONFIG_FILE
 
+# Проверка существования директории нейросети
+if [ ! -d "/home/orangepi/OnnxStream/src/build" ]; then
+    echo "❌ Ошибка: Папка с нейросетью не найдена. Проверь путь."
+    exit 1
+fi
+
 # Генерация изображения
 TIMESTAMP=$(date +%s)
 OUTPUT_FILE="/project/output/generated_$TIMESTAMP.png"
 
-cd OnnxStream/src/build/ || exit 1
+cd /home/orangepi/OnnxStream/src/build/ || exit 1
 
 ./sd --prompt "$PROMPT" --steps 3 --rpi --output "$OUTPUT_FILE"
 
-echo "Генерация завершена. Изображение сохранено в: $OUTPUT_FILE"
+echo "✅ Генерация завершена. Изображение сохранено в: $OUTPUT_FILE"
 
 # Запуск галереи
 bash gallery.sh
