232eee99c8f1491872103781b6d4d5af11b5c1a8
Agafferak <Agaffer321@gmail.com>
2025-04-24 21:17:43 +0300

    Update gallery.sh
    Add feh & remove fbi

--- photoframe/gallery.sh
+++ photoframe/gallery.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-echo "🟢 Старт галереи"
+echo "🖼️ Запуск галереи через feh..."
 
 OUTPUT_DIR="/home/orangepi/project/photoframe/output"
 source ./config.cfg
+
 echo "⏱️ Задержка между изображениями: $DELAY секунд"
 
-while true; do
-  for IMAGE in "$OUTPUT_DIR"/*.png; do
-    if [ -f "$IMAGE" ]; then
-      clear
-      echo "🖼️ Показываем: $IMAGE"
-      fbi -a "$IMAGE" > /dev/null 2>&1
-      sleep "$DELAY"
-    fi
-  done
-done
+feh --fullscreen --slideshow-delay "$DELAY" "$OUTPUT_DIR"
