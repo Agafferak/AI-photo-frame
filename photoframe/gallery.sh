d56f46015f7d41eacd263b22578f69c5ad04f767
Agafferak <Agaffer321@gmail.com>
2025-04-24 21:02:45 +0300

    Update gallery.sh

--- photoframe/gallery.sh
+++ photoframe/gallery.sh
@@ -2,22 +2,17 @@
 
 echo "🟢 Старт галереи"
 
-# Прямо прописываем путь
 OUTPUT_DIR="/home/orangepi/project/photoframe/output"
-echo "📁 Папка для изображений: $OUTPUT_DIR"
-
-# Прочитаем DELAY из config.cfg
 source ./config.cfg
 echo "⏱️ Задержка между изображениями: $DELAY секунд"
 
 while true; do
   for IMAGE in "$OUTPUT_DIR"/*.png; do
     if [ -f "$IMAGE" ]; then
+      clear
       echo "🖼️ Показываем: $IMAGE"
-      fbi -T 1 -a "$IMAGE" > /dev/null 2>&1
+      fbi -a "$IMAGE" > /dev/null 2>&1
       sleep "$DELAY"
-    else
-      echo "⚠️ Нет изображений в папке."
     fi
   done
 done
